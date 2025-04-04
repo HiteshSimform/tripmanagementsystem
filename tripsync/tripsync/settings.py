ca138ff8ec515ed117fe3b34f4961ace687532e7
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-04-04 19:17:18 +0530

    TripSync Project - User Model and Authenticate

--- tripsync/tripsync/settings.py
+++ tripsync/tripsync/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apps.attachments',
     'apps.authentication',
     'apps.dashboard',
@@ -51,6 +52,8 @@ INSTALLED_APPS = [
     'apps.trips',
     'apps.users',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -86,10 +89,21 @@ WSGI_APPLICATION = 'tripsync.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tripsync",
+        "USER": "postgres",
+        "PASSWORD": "hitesh123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
