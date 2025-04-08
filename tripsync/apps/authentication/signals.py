d8fc0782746059a1a9431099dd681d0dec5eebef
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-04-08 11:36:57 +0530

    TripSync Project - Signals implemented for change the user's role During Register the user

--- tripsync/apps/authentication/signals.py
+++ tripsync/apps/authentication/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from apps.users.models import User
-from apps.trips.models import Trip
+# from apps.trips.models import Trip
 
 @receiver(post_save, sender=User)
 def send_welcome_mail(sender, instance, created, **kwargs):
@@ -12,6 +12,7 @@ def send_welcome_mail(sender, instance, created, **kwargs):
         send_mail(
             subject="Welcome to TripSync!",
             message=f"Hi {instance.username}, thank you for registering!",
+            from_email=settings.EMAIL_HOST_USER,
             recipient_list=[user_email],
             fail_silently=False,
         )
@@ -25,15 +26,15 @@ def update_role_on_registration(sender, instance, created, **kwargs):
 
 # 
 
-@receiver(post_save, sender= Trip)
-def update_role_on_trip_creation(sender, instance, created, **kwargs):
-    if created and instance.creator.role in ["Viewer","participant"] :
-        instance.creator.role = "trip_admin"
-        instance.creator.save()
+# @receiver(post_save, sender= Trip)
+# def update_role_on_trip_creation(sender, instance, created, **kwargs):
+#     if created and instance.creator.role in ["Viewer","participant"] :
+#         instance.creator.role = "trip_admin"
+#         instance.creator.save()
 
-@receiver(post_save, sender= Trip)
-def update_role_on_participation(sender,instance,**kwargs):
-    for participant in instance.participants.all():
-        if participant.role != "trip_admin":
-            participant.role = "participant"
-            participant.save()
+# @receiver(post_save, sender= Trip)
+# def update_role_on_participation(sender,instance,**kwargs):
+#     for participant in instance.participants.all():
+#         if participant.role != "trip_admin":
+#             participant.role = "participant"
+#             participant.save()
